8fb0d3cb1943dcaa30e333475f742d0a3c38affa
uuhnaut69 <uuhnaut96@gmail.com>
2021-03-29 09:22:23 +0700

    :rocket: Switch query to entitygraph

--- javer/src/main/java/com/uuhnaut69/javer/repository/RestaurantRepository.java
+++ javer/src/main/java/com/uuhnaut69/javer/repository/RestaurantRepository.java
@@ -1,8 +1,8 @@
 package com.uuhnaut69.javer.repository;
 
 import com.uuhnaut69.javer.model.Restaurant;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +11,6 @@ import java.util.UUID;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
 
-  @Query(value = "select r from restaurants r left join fetch r.dishes")
+  @EntityGraph(attributePaths = {"dishes"})
   List<Restaurant> findAll();
 }
--- kover/src/main/kotlin/com/uuhnaut69/kover/repository/RestaurantRepository.kt
+++ kover/src/main/kotlin/com/uuhnaut69/kover/repository/RestaurantRepository.kt
@@ -1,14 +1,14 @@
 package com.uuhnaut69.kover.repository
 
 import com.uuhnaut69.kover.model.Restaurant
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface RestaurantRepository : JpaRepository<Restaurant, UUID> {
 
-    @Query(value = "select r from restaurants r left join fetch r.dishes")
+    @EntityGraph(attributePaths = ["dishes"])
     override fun findAll(): List<Restaurant>
 }
