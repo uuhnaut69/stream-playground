c3dcb2c5874c0a6c25592247a4e943e371a79386
uuhnaut69 <uuhnaut96@gmail.com>
2021-03-30 21:23:10 +0700

    Rename java kafka streams projects

--- aggregator/java-spring-cloud-streams-aggregator/src/main/java/com/uuhnaut69/jaggregator/JAggregatorApplication.java
+++ aggregator/java-spring-cloud-streams-aggregator/src/main/java/com/uuhnaut69/jaggregator/JAggregatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JaggregatorApplication {
+public class JAggregatorApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(JaggregatorApplication.class, args);
+    SpringApplication.run(JAggregatorApplication.class, args);
   }
 }
